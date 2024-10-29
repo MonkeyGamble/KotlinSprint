cf2141cca7f813bd4c6066ac12c8268a1f5405d4
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-10-29 23:36:00 +0100

    KS-1-3 3rd task solution

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,6 +1,16 @@
 package lesson_1
 
 fun main(){
-    println("Тестовое решение задачи 3")
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
 
+    println("Год полета Юрия Гагарина: $year")
+    println("Час взлета: 0$hour")
+    println("Минута взлета: 0$minute")
+
+    hour = 10
+    minute = 55
+    print("Время посадки: $hour:")
+    print(minute)
 }
