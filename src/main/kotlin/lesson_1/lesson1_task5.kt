ac4dfc2e0c828ba7030f3ea262e62197cb65b3f0
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-10-29 23:56:14 +0100

    KS-1-5 5th task solution refactor

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -4,7 +4,7 @@ fun main(){
     val sec: Int = 6480
     val hours: Int = (sec/3600).toInt()
     val minutes = ((sec - 3600)/60).toInt()
-    val seconds = sec - hours*3600  - minutes*60
+    val seconds = sec - hours*3600 - minutes*60
 
     println("Время, проведенное в космосе: 0$hours:$minutes:0$seconds")
 }
