cd72f1b8dfbb4f266df46f66ab12e476fff3b735
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-10-29 23:27:43 +0100

    KS-1-2 2nd task solution

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,6 +1,14 @@
 package lesson_1
 
 fun main(){
-    println("Тестовое решение задачи 2")
+    val ordersCount: Int = 75
+    val thanksForOrder: String = "Thank you for the order!"
 
+    println("Количество заказов: $ordersCount")
+    println(thanksForOrder)
+
+    var employeesCount: Int = 2000
+//  println("Количество работников: $employeesCount")
+    employeesCount -= 1
+    println("Количество работников: $employeesCount")
 }
