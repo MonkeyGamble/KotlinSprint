ee4ae8ba38b692e3fd6fca5f0b4f77618254ee1d
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-10-30 22:31:56 +0100

    KS-1-5 5th task solution

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,9 +2,9 @@ package lesson_1
 
 fun main(){
     val sec: Int = 6480
-    val hours: Int = (sec/3600).toInt()
-    val minutes = ((sec - 3600)/60).toInt()
-    val seconds = sec - hours*3600 - minutes*60
+    val hours: Int = sec/3600
+    val minutes: Int = (sec - 3600)/60
+    val seconds: Int = sec % 60
 
-    println("Время, проведенное в космосе: 0$hours:$minutes:0$seconds")
+    println("Время, проведенное в космосе: %02d:%02d:%02d".format(hours, minutes, seconds))
 }
