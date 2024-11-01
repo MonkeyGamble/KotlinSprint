6dd37024af6074c590b20bf20872af4ceb27a345
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-11-01 21:13:39 +0100

    KS-1-5 5th task solution corrected

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,13 +1,20 @@
 package lesson_1
 
-fun main() {
-    val secInHour: Int = 3600
-    val secInMin: Int = 60
-    val sec: Int = 6480
+const val SEC_IN_HOUR: Int = 3600
+const val SEC_IN_MIN: Int = 60
 
-    val hours: Int = sec / secInHour
-    val minutes: Int = (sec % secInHour) / secInMin
-    val seconds: Int = sec % secInMin
+fun main() {
+
+    val sec: Int = 6480
+    val hours: Int = sec / SEC_IN_HOUR
+    val minutes: Int = (sec % SEC_IN_HOUR) / SEC_IN_MIN
+    val seconds: Int = sec % SEC_IN_MIN
 
     println("Время, проведенное в космосе: %02d:%02d:%02d".format(hours, minutes, seconds))
 }
+
+
+//Объяви переменную с количеством секунд, которые Гагарин провел в космосе.
+// - Переведи в коде количество секунд в целые минуты и целые часы, сохраняя остаток секунд и минут в новые переменные;
+//- Выведи в консоль время, проведенное в космосе в формате [часы:минуты:секунды], например так: 01:30:09;
+//- Значения часов, минут и секунд отображать двумя цифрами.
