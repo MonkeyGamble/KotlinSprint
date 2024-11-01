ec7f977e1fdaa61098a346e515083e7d5054f4c5
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-11-01 18:52:26 +0100

    KS-3-5 5th task solution

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -4,17 +4,32 @@ fun main() {
     val departureHours: Int = 9
     val departureMinutes: Int = 39
     val travelTime: Int = 457
+    val magicNumber: Int = 60
 
-    val travelHours: Int = travelTime / 60
-    val travelMinutes: Int = travelTime % 60
+//    val travelHours: Int = travelTime / magicNumber
+//    val travelMinutes: Int = travelTime % magicNumber
+//
+//    var arrivalHours: Int = departureHours + travelHours
+//    var arrivalMinutes: Int = departureMinutes + travelMinutes
+//
+//    if (arrivalMinutes > 59) {
+//        arrivalHours += arrivalMinutes / 60
+//        arrivalMinutes %= 60
+//    }
 
-    var arrivalHours: Int = departureHours + travelHours
-    var arrivalMinutes: Int = departureMinutes + travelMinutes
+    val totalMinutesFromDayStart = departureHours * magicNumber + departureMinutes + travelTime
 
-    if (arrivalMinutes > 59) {
-        arrivalHours += arrivalMinutes / 60
-        arrivalMinutes %= 60
-    }
+    val arrivalHours = (totalMinutesFromDayStart / magicNumber) % 24
+    val arrivalMinutes = totalMinutesFromDayStart % magicNumber
 
     println("Время прибытия поезда: $arrivalHours:$arrivalMinutes")
 }
+
+
+//Сайт с расписанием поездов получает данные с сервера.
+//Сервер посылает время выезда и время в пути, а время прибытия вычисляется из них.
+//Сервер прислал данные, что поезд выехал в 9:39 и будет в пути 457 минут.
+//
+//– Создай целочисленные переменные и проинициализируй их этими данными;
+//– Напиши программу, которая обработает данные и подсчитает час и минуту прибытия поезда;
+//– Выведи результат в консоль.
