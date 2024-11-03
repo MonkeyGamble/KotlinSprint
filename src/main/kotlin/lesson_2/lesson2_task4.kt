5fb91113e994a5f00f4e7dbda8f21861d3b72739
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-11-03 20:31:54 +0100

    KS-2-4 4th task solution corrected again

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,12 +1,15 @@
 package lesson_2
 
+const val ONE_HUNDRED: Double = 100.0
+
+
 fun main() {
     val crystall: Int = 7
     val iron: Int = 11
     val perc: Int = 20
 
-    val crystallBuff: Int = (crystall * (perc / 100.0)).toInt()
-    val ironBuff: Int = (iron * (perc / 100.0)).toInt()
+    val crystallBuff: Int = (crystall * (perc / ONE_HUNDRED)).toInt()
+    val ironBuff: Int = (iron * (perc / ONE_HUNDRED)).toInt()
 
     println("Кол-во бонусных материалов кристаллической руды: $crystallBuff")
     println("Кол-во бонусных материалов железной руды: $ironBuff")
