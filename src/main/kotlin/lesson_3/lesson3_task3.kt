5b468a76940c6a8a1d14eed2f45f4d4597ddd3ec
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-11-06 10:54:58 +0100

    KS-3-3 3rd task solution corrected

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -3,11 +3,20 @@ package lesson_3
 fun main() {
     val num = readln().toInt()
 
-    for (i in 1..9) {
-        println("$num x $i = ${num * i}")
-    }
+    println(
+        """
+            $num x 1 = ${num * 1}
+            $num x 2 = ${num * 2}
+            $num x 3 = ${num * 3}
+            $num x 4 = ${num * 4}
+            $num x 5 = ${num * 5}
+            $num x 6 = ${num * 6}
+            $num x 7 = ${num * 7}
+            $num x 8 = ${num * 8}
+            $num x 9 = ${num * 9}
+        """.trimIndent()
+    )
 }
-
 
 //Пишем программу, которая будет помогать учить таблицу умножения.
 // В отдельную переменную будет записываться число (объявить и проинициализировать),
