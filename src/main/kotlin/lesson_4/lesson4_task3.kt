b4558a21520fd07d37331f7bea2643a77f8f9e32
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-11-08 21:20:29 +0100

    KS-4-3 3rd task solution corrected

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,10 +1,12 @@
 package lesson_4
 
+const val IS_SUNNY: Boolean = true
+const val IS_AWNING_OPEN: Boolean = true
+const val HUMIDITY: Int = 20
+const val SEASON: String = "зима"
+
 fun main() {
-    val weatherIsSunny: Boolean = true
-    val tentIsOpen: Boolean = true
-    val humidity: Int = 20
-    val currentSeason: String = "зима"
+
 
     val weatherTodayIsSunny: Boolean = true
     val tentIsOpenNow: Boolean = true
@@ -14,31 +16,10 @@ fun main() {
     println(
         "Благоприятные ли условия сейчас для роста бобовых?" +
                 " ${
-                    weatherTodayIsSunny == weatherIsSunny
-                            && tentIsOpenNow == tentIsOpen
-                            && humidityNow == humidity
-                            && seasonNow != currentSeason
+                    weatherTodayIsSunny == IS_SUNNY
+                            && tentIsOpenNow == IS_AWNING_OPEN
+                            && humidityNow == HUMIDITY
+                            && seasonNow != SEASON
                 }"
     )
 }
-
-
-//На территории агрокомплекса функционирует система автоматического мониторинга условий для роста растений.
-//Напиши программу, которая будет определять, подходят ли текущие условия для успешного роста бобовых культур.
-//
-//Критерии благоприятных условий включают:
-//- наличие солнечной погоды;
-//- открытый тент;
-//- влажность воздуха составляет 20%;
-//- текущее время года - не зима.
-//
-//Программа должна выводить результат анализа условий в формате: “Благоприятные ли условия сейчас для роста бобовых? true/false”
-//
-//Для проверки работы программы, инициализируй переменные следующими значениями:
-//
-//- погода сегодня - солнечная;
-//- тент на данный момент - открыт;
-//- влажность воздуха - 20%;
-//- текущее время года - зима.
-//
-//И типами данных - boolean, boolean, int, String.
